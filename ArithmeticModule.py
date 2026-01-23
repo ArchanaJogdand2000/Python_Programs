2287063ade874c194df1dcf19e221863e73839eb
ArchanaJogdand2000 <archanajogdand24@gmail.com>
2026-01-23 16:06:38 +0530

    Add files via upload

---
+++ ArithmeticModule.py
@@ -0,0 +1,17 @@
+print("inside module",__name__)
+
+def Add(No1,No2):
+    Sum = No1 + No2
+    return Sum
+
+def Sub(No1,No2):
+    Ans = No1 - No2
+    return Ans
+
+def Mult(No1, No2):
+    Ans = No1 * No2
+    return Ans
+
+def Div(No1, No2):
+    Ans = No1 / No2
+    return Ans
